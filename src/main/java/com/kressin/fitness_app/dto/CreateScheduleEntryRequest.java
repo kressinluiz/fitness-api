d3cd230b7a2b68ef13677349a3d9a41da469db6e
luiz kressin <luiz.kressin@gmail.com>
2026-07-19 11:47:28 -0300

    validação de datetime no scheduleentry para impedir agendamento em datas passadas;

--- src/main/java/com/kressin/fitness_app/dto/CreateScheduleEntryRequest.java
+++ src/main/java/com/kressin/fitness_app/dto/CreateScheduleEntryRequest.java
@@ -3,9 +3,10 @@ package com.kressin.fitness_app.dto;
 import java.time.DayOfWeek;
 import java.time.ZonedDateTime;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateScheduleEntryRequest(
         @NotNull DayOfWeek weekDay,
-        @NotNull ZonedDateTime dateTime) {
+        @NotNull @FutureOrPresent ZonedDateTime dateTime) {
 }
--- src/main/java/com/kressin/fitness_app/dto/UpdateScheduleEntryRequest.java
+++ src/main/java/com/kressin/fitness_app/dto/UpdateScheduleEntryRequest.java
@@ -3,10 +3,12 @@ package com.kressin.fitness_app.dto;
 import java.time.DayOfWeek;
 import java.time.ZonedDateTime;
 
+import jakarta.validation.constraints.FutureOrPresent;
+
 public record UpdateScheduleEntryRequest(
         Long id,
         Boolean shouldDelete,
         DayOfWeek weekDay,
-        ZonedDateTime dateTime) {
+        @FutureOrPresent ZonedDateTime dateTime) {
 
 }
